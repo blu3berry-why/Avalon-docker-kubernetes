62a7a20269d9cffb6f33ab600d77a4242e018bea
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2022-04-24 23:10:46 +0200

    changing mongo to stateful set

--- manifests/clear-deployments.bat
+++ manifests/clear-deployments.bat
@@ -2,14 +2,19 @@ kubectl delete deploy avalon-emailhandler
 kubectl delete deploy gamelogic
 kubectl delete deploy mongodb-deployment
 
+kubectl delete statefulset mongoset
+
 kubectl delete secret avalon-secret
 kubectl delete secret mongodb-secret
 
 kubectl delete configmap avalon-configmap
 kubectl delete ingress avalon-ingress
 
+kubectl delete storageclass local-storage
+
 kubectl delete service avalon-emailhandler
 kubectl delete service gamelogic
 kubectl delete service mongodb-service
+kubectl delete service mongo
 
 PAUSE
--- manifests/deploy.bat
+++ manifests/deploy.bat
@@ -1,13 +1,23 @@
+:: configmaps
 kubectl apply -f configmaps\avalon-configmap.yaml
 kubectl apply -f configmaps\mongo-config-map.yaml
 
+:: secrets
 kubectl apply -f secrets\avalon-secret.yaml
 kubectl apply -f secrets\mongo-secret.yaml
 
+:: Deployments
 kubectl apply -f deployment\gamelogic-deployment.yaml
-kubectl apply -f deployment\mongo-deployment.yaml
+REM kubectl apply -f deployment\mongo-deployment.yaml
 kubectl apply -f deployment\email-handler-deployment.yaml
 
-kubectl apply -f ingess\avalon-ingress.yaml
+:: StorageClasses
+kubectl apply -f storageclasses\storageclass.yaml
+
+:: StatefulSets
+kubectl apply -f statefulsets\mongo.yaml
+
+:: Ingress
+kubectl apply -f ingress\avalon-ingress.yaml
 
 PAUSE
