f030ed2acb1760a813a6819d38a58c70baa0b495
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2022-05-18 00:59:33 +0200

    mongo cluster works locally

--- test-manifest/mongo-operator/deploy.bat
+++ test-manifest/mongo-operator/deploy.bat
@@ -2,7 +2,5 @@ kubectl apply -f namespace.yaml
 kubectl apply -f crds/crd.yaml
 kubectl apply -f rbac
 kubectl apply -f operator.yaml
-kubectl apply -f internal/secret.yaml
-kubectl apply -f internal/mongodb.yaml
-
+kubectl apply -f internal/
 PAUSE
